587f537563a8a3369210a2bbf5bb57431dd65cdb
Alistair Galbraith <alistair@alistairs.net>
2018-12-29 17:31:40 -0800

    Updated setup

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+setuptools.setup(
     name = 'pynoon',
-    version = '0.0.1',
+    version = '0.0.2',
     license = 'MIT',
     description = 'Python library for Noon Home',
     author = 'Alistair Galbraith',
     author_email = 'github@alistairs.net',
     url = 'http://github.com/alistairg/pynoon',
     include_package_data=True,
+	packages=setuptools.find_packages(),
     install_requires=['requests', 'websocket-client'],
     classifiers = [
         'Development Status :: 3 - Alpha',
@@ -20,6 +21,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Topic :: Home Automation',
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    zip_safe=True,
+    ]
+
 )
